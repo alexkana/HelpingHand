9078a2d14bb22d4c0374c5463afcf89409000b6e
mickonama <mickonama@csd.auth.gr>
2021-04-18 20:13:21 +0300

    -I saw a red frame and I want it painted black

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+let CURRENT_LANGUAGE = 'el'
+
 function set_head(img, lang) {
     document.getElementById("lang_selector").innerHTML = "<img class=\"flag_icon\" src=\"images/" + img + "\" alt=\"...\">&nbsp" + lang
 }
 
 function trans_navbar(language) {
-    let i1 = 'Αρχική Σελίδα'
-    let i2 = 'Σχετικά με εμάς'
-    let i3 = 'Οργανώσεις'
-    let i4 = 'Επικοινωνία'
-    let i5 = 'Εγγραφή/Σύνδεση'
+    let i1,i2,i3,i4,i5;
     if (language === 'en') {
         i1 = 'Home page'
         i2 = 'About us'
         i3 = 'Organizations'
         i4 = 'Contact'
         i5 = 'Sign-Up/Log-in'
+    }else{
+        i1 = 'Αρχική Σελίδα'
+        i2 = 'Σχετικά με εμάς'
+        i3 = 'Οργανώσεις'
+        i4 = 'Επικοινωνία'
+        i5 = 'Εγγραφή/Σύνδεση'
     }
     document.getElementById("item1").innerHTML = i1;
     document.getElementById("item2").innerHTML = i2;

b817e831176a384cb40c1822f71c0264a3801984
christos-kouros <cekouros@csd.auth.gr>
2021-04-28 17:59:00 +0300

    halping a hand .com

--- scripts/Contact.js
+++ scripts/Contact.js
@@ -11,11 +11,4 @@ function checkLanguageText() {
     }
     document.getElementById("text1").innerHTML = text1;
     document.getElementById("text2").innerHTML = text2;
-
-
-    // document.getElementById("text3").innerHTML = text3;
-    // document.getElementById("text4").innerHTML = text4;
-    // document.getElementById("text5").innerHTML = text5;
-    // document.getElementById("text6").innerHTML = text6;
-
 }

e5b73216c4700d8993abf0daaf5b68d88ae035cc
alexkana <alextube234@gmail.com>
2021-06-14 22:50:05 +0300

    Search bar

--- pages/Organization.php
+++ pages/Organization.php
@@ -103,6 +103,17 @@
 
     }
 </script>
+<div class="search_org">
+<div class="input-group">
+  <div class="form-outline">
+    <input type="search" id="form1" class="form-control" />
+    <label class="form-label" for="form1">Search</label>
+  </div>
+  <button type="button" class="btn btn-primary">
+    <i class="fas fa-search"></i>
+  </button>
+</div>
+</div>
 
 
 
